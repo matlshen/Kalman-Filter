783e70c7a03276eaad29790db5d7a68a8ae62339
Matthew Shen <shenmatthew@outlook.com>
2023-04-26 20:10:36 -0400

    initial

---
+++ KF.py
@@ -0,0 +1,71 @@
+"""
+EEL 4930/5934: Autonomous Robots
+University Of Florida
+"""
+
+import numpy as np
+
+class KF_2D(object):
+    def __init__(self, dt, u_x, u_y, std_acc, x_std_meas, y_std_meas):
+        """
+            dt: sampling time (time for 1 cycle)
+            u_x: acceleration in x-direction
+            u_y: acceleration in y-direction
+            std_acc: process noise magnitude
+            x_std_meas: standard deviation of the measurement in x-direction
+            y_std_meas: standard deviation of the measurement in y-direction
+        """
+        self.dt = dt # sampling time
+        self.u = np.matrix([[u_x],[u_y]]) # control input variables
+        self.x = np.matrix([[0], [0], [0], [0]]) # intial State
+        
+        # State Transition Matrix A (complete the definition)
+        self.A = np.matrix([[1, 0, self.dt, 0],
+                            [0, 1, 0, self.dt],
+                            [0, 0, 1, 0],
+                            [0, 0, 0, 1]])
+
+        # Control Input Matrix B (defined for you)
+        self.B = np.matrix([[(self.dt**2)/2, 0],
+                            [0,(self.dt**2)/2],
+                            [self.dt,0],
+                            [0,self.dt]])
+
+        # Measurement Mapping Matrix (complete the definition)
+        self.H = np.matrix([[1, 0, 0, 0],
+                            [0, 1, 0, 0]])
+
+        # Initial Process Noise Covariance (defined for you)
+        self.Q = np.matrix([[(self.dt**4)/4, 0, (self.dt**3)/2, 0],
+                            [0, (self.dt**4)/4, 0, (self.dt**3)/2],
+                            [(self.dt**3)/2, 0, self.dt**2, 0],
+                            [0, (self.dt**3)/2, 0, self.dt**2]]) * std_acc**2
+
+        #Initial Measurement Noise Covariance (complete the definition)
+        self.R = np.matrix([[x_std_meas**2, 0],
+                            [0, y_std_meas**2]])
+
+        #Initial Covariance Matrix (defined for you)
+        self.P = np.eye(self.A.shape[1])
+
+
+    def predict(self):
+        ## complete this function
+        # Update time state (self.x): x_k =Ax_(k-1) + Bu_(k-1) 
+        self.x = self.A @ self.x + self.B @ self.u
+        # Calculate error covariance (self.P): P= A*P*A' + Q 
+        self.P = self.A @ self.P @ self.A.T + self.Q
+        return self.x[0:2]
+
+
+    def update(self, z):
+        ## complete this function
+        # Calculate S = H*P*H'+R
+        S = self.H @ self.P @ self.H.T + self.R
+        # Calculate the Kalman Gain K = P * H'* inv(H*P*H'+R)
+        K = self.P @ self.H.T @ np.linalg.inv(S)
+        # Update self.x
+        self.x = self.x + K @ (z - self.H @ self.x)
+        # Update error covariance matrix self.P
+        self.P = (np.eye(self.P.shape[0]) - K @ self.H) @ self.P
+        return self.x[0:2]
---
+++ circleTracker.py
@@ -0,0 +1,76 @@
+"""
+EEL 4930/5934: Autonomous Robots
+University Of Florida
+"""
+import cv2
+import numpy as np
+from KF import KF_2D
+
+
+def circleDetector(image):
+    """ Simple OpenCV function for circle detection
+        - detects edges, applies threshold to binary image space 
+        - then find object countours 
+        - then returns the center of the detected circle
+    """
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # get gray image
+    img_edges = cv2.Canny(gray,  50, 190, 3) # detect edges
+    #cv2.imshow('img_edges', img_edges)
+    ret, img_thresh = cv2.threshold(img_edges, 254, 255, cv2.THRESH_BINARY) # convert to binary images
+    #cv2.imshow('img_thresh', img_thresh)
+    contours, _ = cv2.findContours(img_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # get object contours
+
+    # draw contour (see opencv tutorials)
+    min_radius, max_radius = 3, 30
+    centers=[]
+    for c in contours:
+        (x, y), radius = cv2.minEnclosingCircle(c)
+        radius = int(radius)
+        if (radius > min_radius) and (radius < max_radius): # Take only the valid circle(s)
+            centers.append(np.array([[x], [y]]))
+    cv2.imshow('contours', img_thresh)
+    return centers
+
+
+
+# OpenCV video capture object
+VideoCap = cv2.VideoCapture('C:/Users/mshen/OneDrive - University of Florida/Documents/School/EEL 4930 (Robots)/HH5/HW5_Blank/HW5_Blank/data/rBall.avi')
+
+"""
+# Create Kalman Filter object KF
+    dt: sampling time (time for 1 cycle)
+    u_x: acceleration in x-direction
+    u_y: acceleration in y-direction
+    std_acc: process noise magnitude
+    x_std_meas: standard deviation of the measurement in x-direction
+    y_std_meas: standard deviation of the measurement in y-direction
+"""
+filter = KF_2D(dt=0.1, u_x=1, u_y=1, std_acc=1, x_std_meas=0.1, y_std_meas=0.1)
+
+# Track circle (predict + update) using KF
+while(True):
+    ret, frame = VideoCap.read() # Read frame
+    centers = circleDetector(frame) # Detect object
+    # If centroids are detected then track them
+    if (len(centers) > 0):
+        cv2.circle(frame, (int(centers[0][0]), int(centers[0][1])), 10, (0, 191, 255), 2) # draw circle[0]
+        cv2.putText(frame, "Measured Position", (int(centers[0][0] + 15), int(centers[0][1] - 15)), 0, 0.5, (0,191,255), 2)
+
+        # Predict
+        (x, y) = filter.predict()
+        cv2.rectangle(frame, (int(x - 15), int(y - 15)), (int(x + 15), int(y + 15)), (255, 0, 0), 2) # draw a rectangle
+        cv2.putText(frame, "Predicted Position", (int(x + 15), int(y)), 0, 0.5, (255, 0, 0), 2)
+
+        # Update
+        (x1, y1) = filter.update(centers[0])
+        cv2.rectangle(frame, (int(x1 - 15), int(y1 - 15)), (int(x1 + 15), int(y1 + 15)), (0, 0, 255), 2) # draw a rectangle
+        cv2.putText(frame, "Estimated Position", (int(x1 + 15), int(y1 + 10)), 0, 0.5, (0, 0, 255), 2)
+
+    # Show output and wait for keypress
+    cv2.imshow('image', frame)
+    if cv2.waitKey(2) & 0xFF == ord('q'):
+        VideoCap.release()
+        cv2.destroyAllWindows()
+        break
+    cv2.waitKey(25)
+
